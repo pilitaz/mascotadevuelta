5791d09f1589136979dda01b5f15bdd1323da314
pilitaz <jennyp.gomezb@hotmail.com>
2020-11-09 00:49:06 -0500

    borrar carpeta

--- components/petTypes/model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const petTypeSchema = new mongoose.Schema({
-  name: String
-})
-
-const PetType = mongoose.model('pettypes', petTypeSchema)
-
-module.exports = PetType
